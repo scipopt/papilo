f820f5609d961936b0c37ce803e61686c1f4d26b
Leona Gottwald <gottwald@zib.de>
2020-03-05 16:57:37 +0100

    finalize build improvements

--- cmake/Modules/FindTBB.cmake
+++ cmake/Modules/FindTBB.cmake
@@ -18,11 +18,36 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
 
 endif()
 
-# try to find the library
+# try to find the tbb library in the system
 find_library(TBB_LIBRARY
             NAMES tbb libtbb.so.2
             HINTS ${TBB_SEARCH_DIR}
             PATHS ${TBB_DEFAULT_SEARCH_DIR}
             PATH_SUFFIXES ${TBB_LIB_PATH_SUFFIXES})
+
+# if the library was not found try to build a static library from source
+if(NOT TBB_LIBRARY)
+   include(${CMAKE_CURRENT_LIST_DIR}/../../external/tbb/cmake/TBBBuild.cmake)
+   tbb_build(TBB_ROOT ${CMAKE_CURRENT_LIST_DIR}/../../external/tbb CONFIG_DIR TBB_DIR MAKE_ARGS extra_inc=big_iron.inc tbb_build_dir=${CMAKE_CURRENT_BINARY_DIR} tbb_build_prefix=tbb)
+   if(TBB_DIR)
+      # building was successful
+      find_library(TBB_LIBRARY
+         NAMES tbb
+         HINTS
+            ${CMAKE_CURRENT_BINARY_DIR}/tbb_release
+            ${CMAKE_CURRENT_BINARY_DIR}/tbb_debug)
+      if(TBB_LIBRARY)
+         # create an imported target that also links the thread libraries via its interface
+         set(TBB_BUILT_STATIC_LIB 1)
+         find_package( Threads )
+         add_library(tbb STATIC IMPORTED)
+         set_target_properties(tbb PROPERTIES IMPORTED_LOCATION ${TBB_LIBRARY})
+         target_link_libraries(tbb INTERFACE ${CMAKE_THREAD_LIBS_INIT})
+         # add variable to hold the static lib so that it can be installed
+         set(TBB_STATIC_LIB ${TBB_LIBRARY})
+         set(TBB_LIBRARY tbb)
+      endif()
+   endif()
+endif()
 
 find_package_handle_standard_args(TBB REQUIRED_VARS TBB_LIBRARY)
