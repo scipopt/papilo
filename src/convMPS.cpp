6f1e3949099473256eb24f4143fbc6aeae351729
Gabriel Kressin <kressin@zib.de>
2020-06-25 17:16:45 +0200

    script: removed old code which was not problemBuilder

--- src/convMPS.cpp
+++ src/convMPS.cpp
@@ -52,99 +52,10 @@ convMPS( const Problem<double>& prob )
    const int nnz = cm.getNnz();
    const VariableDomains<double> vd = prob.getVariableDomains();
 
-   // Data structures
-   fmt::print( "   // enum declaration, only needed once\n" );
-   fmt::print( "   enum class boundtype{{ kLE, kEq, kGE }};\n" );
-
-   // Variables
-   fmt::print( "   // Variable declaration\n" );
-   fmt::print( "   int nCols = {}; int nRows = {};\n", nCols, nRows );
-   fmt::print( "   Vec<std::string> rownames;\n" );
-   fmt::print( "   Vec<std::string> colnames;\n\n" );
-   fmt::print( "   HashMap<std::string, int> rowname2idx;\n" );
-   fmt::print( "   HashMap<std::string, int> colname2idx;\n" );
-   fmt::print( "   Vec<double> lb4cols;\n" );
-   fmt::print( "   Vec<double> ub4cols;\n" );
-   fmt::print( "   Vec<boundtype> row_type;\n" );
-   fmt::print( "   Vec<RowFlags> row_flags({});\n", nRows );
-   fmt::print( "   Vec<ColFlags> col_flags;\n" );
-   fmt::print( "   Problem<double> problem;\n" );
-
-   // Objective
-   fmt::print( "   // Objective\n" );
-   fmt::print( "   Vec<double> coeffobj{{ " );
-   for( double coeff: obj.coefficients )
-      fmt::print ("{},", coeff );
-   fmt::print( "}};\n" );
-   fmt::print( "   problem.setObjective( coeffobj, {} );\n\n", obj.offset );
-
-   // Constraint Matrix
-   fmt::print( "   // Constraint Matrix\n" );
-   fmt::print( "   // Vec<std::tuple<int, int, double>> entries{{" );
-   // iterate through every row and add columns
-   for( int r = 0; r < nRows; ++r )
-   {
-      const SparseVectorView<double>& rc = cm.getRowCoefficients( r );
-      const int len = rc.getLength();
-      const int* indices = rc.getIndices();
-      const double* vals = rc.getValues();
-      for(int i = 0; i < len; ++i)
-         fmt::print( "{{{},{},{}}},", r, *(indices + i), *(vals + i) );
-   }
-   fmt::print( "}};\n" );
-   // iterate through every row and set lhs and rhs
-   fmt::print( "   Vec<double> rowlhs{{" );
-   for( int r = 0; r < nRows; ++r )
-      fmt::print( "{},", rowlhs[r] );
-   fmt::print( "   }};\n" );
-   fmt::print( "   Vec<double> rowrhs{{" );
-   for( int r = 0; r < nRows; ++r )
-      fmt::print( "{},", rowrhs[r] );
-   fmt::print( "   }};\n" );
-   // Rowflags
-   fmt::print( "   " );
-   for( int r = 0; r < nRows; ++r )
-   {
-      fmt::print( "row_flags[{}].set(", r );
-
-      if( row_flags[r].test( RowFlag::NONE ) )
-         fmt::print( "RowFlag::NONE" );
-      else
-      {
-         bool somethingset = false;
-         if( row_flags[r].test( RowFlag::kLhsInf ) )
-         {
-            fmt::print( "RowFlag::kLhsInf" );
-            somethingset = true;
-         }
-         if( row_flags[r].test( RowFlag::kRhsInf ) )
-         {
-            somethingset ? fmt::print( ",RowFlag::kRhsInf" ) : fmt::print( "RowFlag::kRhsInf" );
-            somethingset = true;
-         }
-         if( row_flags[r].test( RowFlag::kEquation ) )
-         {
-            somethingset ? fmt::print( ",RowFlag::kEquation" ) : fmt::print( "RowFlag::kEquation" );
-            somethingset = true;
-         }
-         if( row_flags[r].test( RowFlag::kIntegral ) )
-         {
-            somethingset ? fmt::print( ",RowFlag::kIntegral" ) : fmt::print( "RowFlag::kIntegral" );
-            somethingset = true;
-         }
-         assert( somethingset == true );
-      }
-
-      fmt::print( ");" ); // no newline so the file does not get clustered with rowflags...
-   }
-   fmt::print( "\n" );
-   fmt::print( "   problem.setConstraintMatrix( SparseStorage<double>{{ entries, nCols, nRows, false }} , rowlhs, rowrhs, row_flags, false );" );
-
-   // Version 2: The problem builder
+   // Start printing
    fmt::print( "\n\n\n   ///PROBLEM BUILDER CODE\n" );
    fmt::print( "   int nCols = {}; int nRows = {};\n", nCols, nRows );
    fmt::print( "   ProblemBuilder<double> pb;\n" );
-
    // Set all needed things
    fmt::print( "   pb.reserve( {},{},{} );\n", nnz, nRows, nCols );
    // Obj
