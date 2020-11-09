0b7366a359989893f3bab504006c1bec06f43bb6
Alexander Hoen <hoen@zib.de>
2020-11-09 13:44:01 +0100

    fix and refactor tests for SparseStorage

--- test/SparseStorage.cpp
+++ test/SparseStorage.cpp
@@ -21,13 +21,300 @@
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#define CATCH_CONFIG_MAIN
 #include "papilo/core/SparseStorage.hpp"
 #include "catch/catch.hpp"
 #include "papilo/misc/compress_vector.hpp"
 
-TEST_CASE( "sparse storage can be created from triplets and compressed",
-           "[core]" )
+papilo::SparseStorage<double>
+setupSparseMatrix();
+
+TEST_CASE( "sparse storage can be created from triplets", "[core]" )
+{
+   papilo::SparseStorage<double> matrix = setupSparseMatrix();
+
+   REQUIRE( matrix.getNnz() == 13 );
+   REQUIRE( matrix.getNRows() == 5 );
+   REQUIRE( matrix.getNCols() == 9 );
+
+   int expectedRowRanges[5] = { 2, 5, 1, 0, 5 };
+
+   auto rowRanges = matrix.getRowRanges();
+   auto rowValues = matrix.getValues();
+   auto columns = matrix.getColumns();
+
+   for( int i = 0; i < 5; ++i )
+   {
+      REQUIRE( rowRanges[i].end - rowRanges[i].start == expectedRowRanges[i] );
+      double* row = rowValues + rowRanges[i].start;
+      int* rowColumns = columns + rowRanges[i].start;
+      switch( i )
+      {
+      case 0:
+         REQUIRE( row[0] == 1.0 );
+         REQUIRE( row[1] == 2.0 );
+         REQUIRE( rowColumns[0] == 0 );
+         REQUIRE( rowColumns[1] == 1 );
+         break;
+      case 1:
+         REQUIRE( row[0] == 3.0 );
+         REQUIRE( row[1] == 4.0 );
+         REQUIRE( row[2] == 5.0 );
+         REQUIRE( row[3] == 6.0 );
+         REQUIRE( row[4] == 7.0 );
+         REQUIRE( rowColumns[0] == 1 );
+         REQUIRE( rowColumns[1] == 2 );
+         REQUIRE( rowColumns[2] == 3 );
+         REQUIRE( rowColumns[3] == 4 );
+         REQUIRE( rowColumns[4] == 5 );
+         break;
+      case 2:
+         REQUIRE( row[0] == 8.0 );
+         REQUIRE( rowColumns[0] == 1 );
+         break;
+      case 3:
+         continue;
+      case 4:
+         REQUIRE( row[0] == 9.0 );
+         REQUIRE( row[1] == 10.0 );
+         REQUIRE( row[2] == 11.0 );
+         REQUIRE( row[3] == 12.0 );
+         REQUIRE( row[4] == 13.0 );
+         REQUIRE( rowColumns[0] == 0 );
+         REQUIRE( rowColumns[1] == 1 );
+         REQUIRE( rowColumns[2] == 2 );
+         REQUIRE( rowColumns[3] == 7 );
+         REQUIRE( rowColumns[4] == 8 );
+         break;
+      default:
+         REQUIRE_FALSE( "case default should not be reached" );
+      }
+      // todo check the rowValues and columns are correct
+   }
+}
+
+TEST_CASE( "sparse matrix can be compressed", "[core]" )
+{
+   papilo::SparseStorage<double> matrix = setupSparseMatrix();
+
+   papilo::Vec<int> rowSizes = { 2, 5, 1, -1, 5 };
+   papilo::Vec<int> columnSizes = { 2, 4, 2, 1, 1, 1, -1, 1, 1 };
+   papilo::Vec<int> col_mapping =
+       matrix.compress( rowSizes, columnSizes, false );
+
+   papilo::Vec<int> expectedValues = { 0, 1, 2, 3, 4, 5, -1, 6, 7 };
+   REQUIRE( col_mapping == expectedValues );
+
+   papilo::compress_vector( col_mapping, columnSizes );
+
+   auto rowRanges = matrix.getRowRanges();
+   auto rowValues = matrix.getValues();
+   auto columns = matrix.getColumns();
+
+   int expectedRowRanges[4] = { 2, 5, 1, 5 };
+
+   for( int i = 0; i < 4; ++i )
+   {
+      REQUIRE( rowRanges[i].end - rowRanges[i].start == expectedRowRanges[i] );
+      double* row = rowValues + rowRanges[i].start;
+      int* rowColumns = columns + rowRanges[i].start;
+      switch( i )
+      {
+      case 0:
+         REQUIRE( row[0] == 1.0 );
+         REQUIRE( row[1] == 2.0 );
+         REQUIRE( rowColumns[0] == 0 );
+         REQUIRE( rowColumns[1] == 1 );
+         break;
+      case 1:
+         REQUIRE( row[0] == 3.0 );
+         REQUIRE( row[1] == 4.0 );
+         REQUIRE( row[2] == 5.0 );
+         REQUIRE( row[3] == 6.0 );
+         REQUIRE( row[4] == 7.0 );
+         REQUIRE( rowColumns[0] == 1 );
+         REQUIRE( rowColumns[1] == 2 );
+         REQUIRE( rowColumns[2] == 3 );
+         REQUIRE( rowColumns[3] == 4 );
+         REQUIRE( rowColumns[4] == 5 );
+         break;
+      case 2:
+         REQUIRE( row[0] == 8.0 );
+         REQUIRE( rowColumns[0] == 1 );
+         break;
+      case 3:
+         REQUIRE( row[0] == 9.0 );
+         REQUIRE( row[1] == 10.0 );
+         REQUIRE( row[2] == 11.0 );
+         REQUIRE( row[3] == 12.0 );
+         REQUIRE( row[4] == 13.0 );
+         REQUIRE( rowColumns[0] == 0 );
+         REQUIRE( rowColumns[1] == 1 );
+         REQUIRE( rowColumns[2] == 2 );
+         REQUIRE( rowColumns[3] == 6 );
+         REQUIRE( rowColumns[4] == 7 );
+         break;
+      default:
+         REQUIRE_FALSE( "case default should not be reached" );
+      }
+
+      // todo check the rowValues and columns are correct
+   }
+}
+
+TEST_CASE(
+    "sparse storage can be created from triplets and compressed on transpose",
+    "[core]" )
+{
+   papilo::SparseStorage<double> transpose = setupSparseMatrix().getTranspose();
+
+   // TODO: getNnz refactor to getNonzeros??
+   REQUIRE( transpose.getNnz() == 13 );
+   REQUIRE( transpose.getNRows() == 9 );
+   REQUIRE( transpose.getNCols() == 5 );
+
+   auto columnRanges = transpose.getRowRanges();
+   auto columnValues = transpose.getValues();
+   auto rows = transpose.getColumns();
+
+   int expectedRowRanges[9] = { 2, 4, 2, 1, 1, 1, 0, 1, 1 };
+
+   for( int i = 0; i < 9; ++i )
+   {
+      REQUIRE( columnRanges[i].end - columnRanges[i].start ==
+               expectedRowRanges[i] );
+      double* col = columnValues + columnRanges[i].start;
+      int* columnRows = rows + columnRanges[i].start;
+      switch( i )
+      {
+      case 0:
+         REQUIRE( col[0] == 1.0 );
+         REQUIRE( col[1] == 9.0 );
+         REQUIRE( columnRows[0] == 0 );
+         REQUIRE( columnRows[1] == 4 );
+         break;
+      case 1:
+         REQUIRE( col[0] == 2.0 );
+         REQUIRE( col[1] == 3.0 );
+         REQUIRE( col[2] == 8.0 );
+         REQUIRE( col[3] == 10.0 );
+         REQUIRE( columnRows[0] == 0 );
+         REQUIRE( columnRows[1] == 1 );
+         REQUIRE( columnRows[2] == 2 );
+         REQUIRE( columnRows[3] == 4 );
+         break;
+      case 2:
+         REQUIRE( col[0] == 4.0 );
+         REQUIRE( col[1] == 11.0 );
+         REQUIRE( columnRows[0] == 1 );
+         REQUIRE( columnRows[1] == 4 );
+         break;
+      case 3:
+         REQUIRE( col[0] == 5.0 );
+         REQUIRE( columnRows[0] == 1 );
+         break;
+      case 4:
+         REQUIRE( col[0] == 6.0 );
+         REQUIRE( columnRows[0] == 1 );
+         break;
+      case 5:
+         REQUIRE( col[0] == 7.0 );
+         REQUIRE( columnRows[0] == 1 );
+         break;
+      case 6:
+         continue;
+      case 7:
+         REQUIRE( col[0] == 12.0 );
+         REQUIRE( columnRows[0] == 4 );
+         break;
+      case 8:
+         REQUIRE( col[0] == 13.0 );
+         REQUIRE( columnRows[0] == 4 );
+         break;
+      default:
+         REQUIRE_FALSE( "case default should not be reached" );
+      }
+   }
+}
+
+TEST_CASE( "transposed sparse matrix can be compressed", "[core]" )
+{
+   papilo::SparseStorage<double> transpose =setupSparseMatrix().getTranspose();
+
+   papilo::Vec<int> columnSizes = { 2, 5, 1, -1, 5 };
+   papilo::Vec<int> rowSizes = { 2, 4, 2, 1, 1, 1, -1, 1, 1 };
+   papilo::Vec<int> col_mapping =
+       transpose.compress( rowSizes, columnSizes, true );
+
+   papilo::Vec<int> expectedValues = { 0, 1, 2, -1, 3 };
+   REQUIRE( col_mapping == expectedValues );
+
+   papilo::compress_vector( col_mapping, columnSizes);
+
+   auto rowRanges = transpose.getRowRanges();
+   auto columnValues = transpose.getValues();
+   auto rows = transpose.getColumns();
+
+   int expectedRowRanges[8] = { 2, 4, 2, 1, 1, 1, 1, 1 };
+
+
+   for( int i = 0; i < 8; ++i )
+   {
+      int nonzerosEntriesInRow = rowRanges[i].end - rowRanges[i].start;
+      REQUIRE( nonzerosEntriesInRow == expectedRowRanges[i] );
+      double* col = columnValues + rowRanges[i].start;
+      int* colRows = rows + rowRanges[i].start;
+      switch( i )
+      {
+      case 0:
+         REQUIRE( col[0] == 1.0 );
+         REQUIRE( col[1] == 9.0 );
+         REQUIRE( colRows[0] == 0 );
+         REQUIRE( colRows[1] == 3 );
+         break;
+      case 1:
+         REQUIRE( col[0] == 2.0 );
+         REQUIRE( col[1] == 3.0 );
+         REQUIRE( col[2] == 8.0 );
+         REQUIRE( col[3] == 10.0 );
+         REQUIRE( colRows[0] == 0 );
+         REQUIRE( colRows[1] == 1 );
+         REQUIRE( colRows[2] == 2 );
+         REQUIRE( colRows[3] == 3 );
+         break;
+      case 2:
+         REQUIRE( col[0] == 4.0 );
+         REQUIRE( col[1] == 11.0 );
+         REQUIRE( colRows[0] == 1 );
+         REQUIRE( colRows[1] == 3 );
+         break;
+      case 3:
+         REQUIRE( col[0] == 5.0 );
+         REQUIRE( colRows[0] == 1 );
+         break;
+      case 4:
+         REQUIRE( col[0] == 6.0 );
+         REQUIRE( colRows[0] == 1 );
+         break;
+      case 5:
+         REQUIRE( col[0] == 7.0 );
+         REQUIRE( colRows[0] == 1 );
+         break;
+      case 6:
+         REQUIRE( col[0] == 12.0 );
+         REQUIRE( colRows[0] == 3 );
+         break;
+      case 7:
+         REQUIRE( col[0] == 13.0 );
+         REQUIRE( colRows[0] == 3 );
+         break;
+      default:
+         REQUIRE_FALSE( "case default should not be reached" );
+      }
+   }
+}
+
+papilo::SparseStorage<double>
+setupSparseMatrix()
 {
    // build the triplets for the following matrix:
    // 1  2  0  0  0  0  0  0  0
@@ -35,271 +322,29 @@ TEST_CASE( "sparse storage can be created from triplets and compressed",
    // 0  8  0  0  0  0  0  0  0
    // 0  0  0  0  0  0  0  0  0
    // 9  10 11 0  0  0  0  12 13
-   int nrows = 5;
-   int ncols = 9;
-   Vec<Triplet<double>> triplets = {
-       Triplet<double>{0, 0, 1.0},  Triplet<double>{0, 1, 2.0},
-       Triplet<double>{1, 1, 3.0},  Triplet<double>{1, 2, 4.0},
-       Triplet<double>{1, 3, 5.0},  Triplet<double>{1, 4, 6.0},
-       Triplet<double>{1, 5, 7.0},  Triplet<double>{2, 1, 8.0},
-       Triplet<double>{4, 0, 9.0},  Triplet<double>{4, 1, 10.0},
-       Triplet<double>{4, 2, 11.0}, Triplet<double>{4, 7, 12.0},
-       Triplet<double>{4, 8, 13.0}};
+   int numberRows = 5;
+   int numberColumns = 9;
 
-   Vec<int> rowsize = {2, 5, 1, -1, 5};
-   Vec<int> colsize = {2, 4, 2, 1, 1, 1, -1, 1, 1};
-   SparseStorage<double> matrix{triplets, nrows, ncols, true};
-   SparseStorage<double> transpose = matrix.getTranspose();
+   // TODO: Rename Triplet to IndicesToMatrixValue?
+   papilo::Vec<papilo::Triplet<double>> triplets = {
+       papilo::Triplet<double>{ 0, 0, 1.0 },
+       papilo::Triplet<double>{ 0, 1, 2.0 },
+       papilo::Triplet<double>{ 1, 1, 3.0 },
+       papilo::Triplet<double>{ 1, 2, 4.0 },
+       papilo::Triplet<double>{ 1, 3, 5.0 },
+       papilo::Triplet<double>{ 1, 4, 6.0 },
+       papilo::Triplet<double>{ 1, 5, 7.0 },
+       papilo::Triplet<double>{ 2, 1, 8.0 },
+       papilo::Triplet<double>{ 4, 0, 9.0 },
+       papilo::Triplet<double>{ 4, 1, 10.0 },
+       papilo::Triplet<double>{ 4, 2, 11.0 },
+       papilo::Triplet<double>{ 4, 7, 12.0 },
+       papilo::Triplet<double>{ 4, 8, 13.0 } };
 
-   REQUIRE( matrix.getNnz() == triplets.size() );
-   REQUIRE( matrix.getNRows() == nrows );
-   REQUIRE( matrix.getNCols() == ncols );
-
-   REQUIRE( transpose.getNnz() == triplets.size() );
-   REQUIRE( transpose.getNRows() == ncols );
-   REQUIRE( transpose.getNCols() == nrows );
-
-   auto rowranges = matrix.getRowRanges();
-   auto rowvalues = matrix.getValues();
-   auto columns = matrix.getColumns();
-   for( int i = 0; i < nrows; ++i )
-   {
-      REQUIRE( rowranges[i].end - rowranges[i].start == rowsize[i] );
-      double* row = rowvalues + rowranges[i].start;
-      int* rowcols = columns + rowranges[i].start;
-      switch( i )
-      {
-      case 0:
-         REQUIRE( row[0] == 1.0 );
-         REQUIRE( row[1] == 2.0 );
-         REQUIRE( rowcols[0] == 0 );
-         REQUIRE( rowcols[1] == 1 );
-         break;
-      case 1:
-         REQUIRE( row[0] == 3.0 );
-         REQUIRE( row[1] == 4.0 );
-         REQUIRE( row[2] == 5.0 );
-         REQUIRE( row[3] == 6.0 );
-         REQUIRE( row[4] == 7.0 );
-         REQUIRE( rowcols[0] == 1 );
-         REQUIRE( rowcols[1] == 2 );
-         REQUIRE( rowcols[2] == 3 );
-         REQUIRE( rowcols[3] == 4 );
-         REQUIRE( rowcols[4] == 5 );
-         break;
-      case 2:
-         REQUIRE( row[0] == 8.0 );
-         REQUIRE( rowcols[0] == 1 );
-         break;
-      case 3:
-         continue;
-      case 4:
-         REQUIRE( row[0] == 9.0 );
-         REQUIRE( row[1] == 10.0 );
-         REQUIRE( row[2] == 11.0 );
-         REQUIRE( row[3] == 12.0 );
-         REQUIRE( row[4] == 13.0 );
-         REQUIRE( rowcols[0] == 0 );
-         REQUIRE( rowcols[1] == 1 );
-         REQUIRE( rowcols[2] == 2 );
-         REQUIRE( rowcols[3] == 7 );
-         REQUIRE( rowcols[4] == 8 );
-      }
-
-      // todo check the rowvalues and columns are correct
-   }
-
-   auto colranges = transpose.getRowRanges();
-   auto colvalues = transpose.getValues();
-   auto rows = transpose.getColumns();
-   for( int i = 0; i < ncols; ++i )
-   {
-      REQUIRE( colranges[i].end - colranges[i].start == colsize[i] );
-      double* col = colvalues + colranges[i].start;
-      int* colrows = rows + colranges[i].start;
-      switch( i )
-      {
-      case 0:
-         REQUIRE( col[0] == 1.0 );
-         REQUIRE( col[1] == 9.0 );
-         REQUIRE( colrows[0] == 0 );
-         REQUIRE( colrows[1] == 4 );
-         break;
-      case 1:
-         REQUIRE( col[0] == 2.0 );
-         REQUIRE( col[1] == 3.0 );
-         REQUIRE( col[2] == 8.0 );
-         REQUIRE( col[3] == 10.0 );
-         REQUIRE( colrows[0] == 0 );
-         REQUIRE( colrows[1] == 1 );
-         REQUIRE( colrows[2] == 2 );
-         REQUIRE( colrows[3] == 4 );
-         break;
-      case 2:
-         REQUIRE( col[0] == 4.0 );
-         REQUIRE( col[1] == 11.0 );
-         REQUIRE( colrows[0] == 1 );
-         REQUIRE( colrows[1] == 4 );
-         break;
-      case 3:
-         REQUIRE( col[0] == 5.0 );
-         REQUIRE( colrows[0] == 1 );
-         break;
-      case 4:
-         REQUIRE( col[0] == 6.0 );
-         REQUIRE( colrows[0] == 1 );
-         break;
-      case 5:
-         REQUIRE( col[0] == 7.0 );
-         REQUIRE( colrows[0] == 1 );
-         break;
-      case 6:
-         continue;
-      case 7:
-         REQUIRE( col[0] == 12.0 );
-         REQUIRE( colrows[0] == 4 );
-         break;
-      case 8:
-         REQUIRE( col[0] == 13.0 );
-         REQUIRE( colrows[0] == 4 );
-         break;
-      }
-   }
-
-   Vec<int> col_mapping = matrix.compress( colsize );
-   Vec<int> row_mapping = transpose.compress( rowsize );
-
-   REQUIRE( col_mapping.size() == ncols );
-   REQUIRE( row_mapping.size() == nrows );
-
-   /* check if empty row at index 3 was removed properly */
-   for( int i = 0; i < nrows; ++i )
-   {
-      if( i < 3 )
-         REQUIRE( row_mapping[i] == i );
-      else if( i == 3 )
-         REQUIRE( row_mapping[i] == -1 );
-      else
-         REQUIRE( row_mapping[i] == i - 1 );
-   }
-
-   /* check if empty col at index 6 was removed properly */
-   for( int i = 0; i < ncols; ++i )
-   {
-      if( i < 6 )
-         REQUIRE( col_mapping[i] == i );
-      else if( i == 6 )
-         REQUIRE( col_mapping[i] == -1 );
-      else
-         REQUIRE( col_mapping[i] == i - 1 );
-   }
-
-   --nrows;
-   --ncols;
-
-   compress_vector( col_mapping, colsize );
-   compress_vector( row_mapping, rowsize );
-
-   rowranges = matrix.getRowRanges();
-   rowvalues = matrix.getValues();
-   columns = matrix.getColumns();
-   for( int i = 0; i < nrows; ++i )
-   {
-      REQUIRE( rowranges[i].end - rowranges[i].start == rowsize[i] );
-      double* row = rowvalues + rowranges[i].start;
-      int* rowcols = columns + rowranges[i].start;
-      switch( i )
-      {
-      case 0:
-         REQUIRE( row[0] == 1.0 );
-         REQUIRE( row[1] == 2.0 );
-         REQUIRE( rowcols[0] == 0 );
-         REQUIRE( rowcols[1] == 1 );
-         break;
-      case 1:
-         REQUIRE( row[0] == 3.0 );
-         REQUIRE( row[1] == 4.0 );
-         REQUIRE( row[2] == 5.0 );
-         REQUIRE( row[3] == 6.0 );
-         REQUIRE( row[4] == 7.0 );
-         REQUIRE( rowcols[0] == 1 );
-         REQUIRE( rowcols[1] == 2 );
-         REQUIRE( rowcols[2] == 3 );
-         REQUIRE( rowcols[3] == 4 );
-         REQUIRE( rowcols[4] == 5 );
-         break;
-      case 2:
-         REQUIRE( row[0] == 8.0 );
-         REQUIRE( rowcols[0] == 1 );
-         break;
-      case 3:
-         REQUIRE( row[0] == 9.0 );
-         REQUIRE( row[1] == 10.0 );
-         REQUIRE( row[2] == 11.0 );
-         REQUIRE( row[3] == 12.0 );
-         REQUIRE( row[4] == 13.0 );
-         REQUIRE( rowcols[0] == 0 );
-         REQUIRE( rowcols[1] == 1 );
-         REQUIRE( rowcols[2] == 2 );
-         REQUIRE( rowcols[3] == 6 );
-         REQUIRE( rowcols[4] == 7 );
-      }
-
-      // todo check the rowvalues and columns are correct
-   }
-
-   colranges = transpose.getRowRanges();
-   colvalues = transpose.getValues();
-   rows = transpose.getColumns();
-   for( int i = 0; i < ncols; ++i )
-   {
-      REQUIRE( colranges[i].end - colranges[i].start == colsize[i] );
-      double* col = colvalues + colranges[i].start;
-      int* colrows = rows + colranges[i].start;
-      switch( i )
-      {
-      case 0:
-         REQUIRE( col[0] == 1.0 );
-         REQUIRE( col[1] == 9.0 );
-         REQUIRE( colrows[0] == 0 );
-         REQUIRE( colrows[1] == 3 );
-         break;
-      case 1:
-         REQUIRE( col[0] == 2.0 );
-         REQUIRE( col[1] == 3.0 );
-         REQUIRE( col[2] == 8.0 );
-         REQUIRE( col[3] == 10.0 );
-         REQUIRE( colrows[0] == 0 );
-         REQUIRE( colrows[1] == 1 );
-         REQUIRE( colrows[2] == 2 );
-         REQUIRE( colrows[3] == 3 );
-         break;
-      case 2:
-         REQUIRE( col[0] == 4.0 );
-         REQUIRE( col[1] == 11.0 );
-         REQUIRE( colrows[0] == 1 );
-         REQUIRE( colrows[1] == 3 );
-         break;
-      case 3:
-         REQUIRE( col[0] == 5.0 );
-         REQUIRE( colrows[0] == 1 );
-         break;
-      case 4:
-         REQUIRE( col[0] == 6.0 );
-         REQUIRE( colrows[0] == 1 );
-         break;
-      case 5:
-         REQUIRE( col[0] == 7.0 );
-         REQUIRE( colrows[0] == 1 );
-         break;
-      case 6:
-         REQUIRE( col[0] == 12.0 );
-         REQUIRE( colrows[0] == 3 );
-         break;
-      case 7:
-         REQUIRE( col[0] == 13.0 );
-         REQUIRE( colrows[0] == 3 );
-         break;
-      }
-   }
+   // TODO: warum defineirt man rowSize als negativ
+   papilo::Vec<int> rowSize = { 2, 5, 1, -1, 5 };
+   papilo::Vec<int> colSize = { 2, 4, 2, 1, 1, 1, -1, 1, 1 };
+   papilo::SparseStorage<double> matrix{ triplets, numberRows, numberColumns,
+                                         true };
+   return matrix;
 }
