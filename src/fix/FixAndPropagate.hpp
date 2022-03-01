139506d5da1ebfa2c408cf828fa9009857bc00dc
Alexander Hoen <hoen@zib.de>
2022-03-01 21:12:03 +0100

    fix integer variables first

--- src/fix/FixAndPropagate.hpp
+++ src/fix/FixAndPropagate.hpp
@@ -323,7 +323,7 @@ class FixAndPropagate
 
          if( is_integer != consider_int_or_cont )
          {
-            if(!is_integer and num.isEq( upperBounds[i], lowerBounds[i] ) && !ub_inf && !lb_inf )
+            if(!is_integer || num.isEq( upperBounds[i], lowerBounds[i] ) || ub_inf || lb_inf )
                unfixed_cont = true;
             continue;
          }
