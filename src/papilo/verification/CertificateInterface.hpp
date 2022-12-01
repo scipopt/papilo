2c10791323b113b92fba24ad5d72665621532adb
Alexander Hoen <hoen@zib.de>
2022-12-01 14:49:31 +0100

    use an abstract class pt2

---
+++ src/papilo/verification/CertificateInterface.hpp
@@ -0,0 +1,74 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*               This file is part of the program and library                */
+/*    PaPILO --- Parallel Presolve for Integer and Linear Optimization       */
+/*                                                                           */
+/* Copyright (C) 2020-2022 Konrad-Zuse-Zentrum                               */
+/*                     fuer Informationstechnik Berlin                       */
+/*                                                                           */
+/* This program is free software: you can redistribute it and/or modify      */
+/* it under the terms of the GNU Lesser General Public License as published  */
+/* by the Free Software Foundation, either version 3 of the License, or      */
+/* (at your option) any later version.                                       */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU Lesser General Public License for more details.                       */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this program.  If not, see <https://www.gnu.org/licenses/>.    */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef _PAPILO_VERI_CERTIFICATE_INTERFACE_HPP_
+#define _PAPILO_VERI_CERTIFICATE_INTERFACE_HPP_
+
+#include "papilo/core/Problem.hpp"
+#include "papilo/misc/Num.hpp"
+#include "papilo/misc/Vec.hpp"
+#include "papilo/misc/compress_vector.hpp"
+#include "papilo/misc/fmt.hpp"
+#include "papilo/verification/ArgumentType.hpp"
+
+namespace papilo
+{
+
+/// type to store necessary data for post solve
+template <typename REAL>
+class CertificateInterface
+{
+
+ public:
+   CertificateInterface() = default;
+
+   virtual void
+   print_header() = 0;
+
+   virtual void
+   change_upper_bound( REAL val, const String& name,
+                       ArgumentType argument = ArgumentType::kPrimal ) = 0;
+
+   virtual void
+   change_lower_bound( REAL val, const String& name,
+                       ArgumentType argument = ArgumentType::kPrimal ) = 0;
+
+   virtual void
+   change_rhs( int row, REAL val, const SparseVectorView<REAL>& data,
+               const Vec<String>& names, const Vec<int>& var_mapping ) = 0;
+
+   virtual void
+   change_lhs( int row, REAL val, const SparseVectorView<REAL>& data,
+               const Vec<String>& names, const Vec<int>& var_mapping ) = 0;
+
+   virtual void
+   mark_row_redundant( int row ) = 0;
+
+   virtual void
+   compress( const Vec<int>& rowmapping, const Vec<int>& colmapping,
+             bool full = false ) = 0;
+};
+
+} // namespace papilo
+
+#endif
---
+++ src/papilo/verification/EmptyCertificate.hpp
@@ -0,0 +1,89 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*               This file is part of the program and library                */
+/*    PaPILO --- Parallel Presolve for Integer and Linear Optimization       */
+/*                                                                           */
+/* Copyright (C) 2020-2022 Konrad-Zuse-Zentrum                               */
+/*                     fuer Informationstechnik Berlin                       */
+/*                                                                           */
+/* This program is free software: you can redistribute it and/or modify      */
+/* it under the terms of the GNU Lesser General Public License as published  */
+/* by the Free Software Foundation, either version 3 of the License, or      */
+/* (at your option) any later version.                                       */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU Lesser General Public License for more details.                       */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this program.  If not, see <https://www.gnu.org/licenses/>.    */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef _PAPILO_VERI_EMPTY_CERTIFICATE_HPP_
+#define _PAPILO_VERI_EMPTY_CERTIFICATE_HPP_
+
+
+#include "papilo/verification/CertificateInterface.hpp"
+
+namespace papilo
+{
+
+
+/// type to store necessary data for post solve
+template <typename REAL>
+class EmptyCertificate : public CertificateInterface<REAL>
+{
+ public:
+
+   EmptyCertificate() = default;
+
+   void
+   print_header()
+   {
+   };
+
+   void
+   change_upper_bound( REAL val, const String& name,
+                       ArgumentType argument = ArgumentType::kPrimal )
+   {
+
+   }
+
+   void
+   change_lower_bound(  REAL val, const String& name, ArgumentType argument = ArgumentType::kPrimal)
+   {
+   }
+
+   void
+   change_rhs( int row, REAL val, const SparseVectorView<REAL>& data,
+               const Vec<String>& names, const Vec<int>& var_mapping )
+   {
+   }
+
+   void
+   change_lhs( int row, REAL val, const SparseVectorView<REAL>& data,
+               const Vec<String>& names, const Vec<int>& var_mapping )
+   {
+   }
+
+   void
+   mark_row_redundant( int row )
+   {
+   }
+
+   void
+   compress( const Vec<int>& rowmapping, const Vec<int>& colmapping,
+             bool full = false )
+   {
+   }
+
+
+};
+
+
+
+} // namespace papilo
+
+#endif
