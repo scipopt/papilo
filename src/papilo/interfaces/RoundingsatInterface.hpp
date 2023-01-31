962ddaf1c173de50b029e39ed69f80cfdf305aba
Alexander Hoen <hoen@zib.de>
2023-01-31 23:08:13 +0100

    RoundingSat: implement doSetUp function WIP

--- src/papilo/interfaces/RoundingsatInterface.hpp
+++ src/papilo/interfaces/RoundingsatInterface.hpp
@@ -1,119 +1,262 @@
-/***********************************************************************
-Copyright (c) 2014-2020, Jan Elffers
-Copyright (c) 2019-2021, Jo Devriendt
-Copyright (c) 2020-2021, Stephan Gocht
-Copyright (c) 2014-2021, Jakob Nordström
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*               This file is part of the program and library                */
+/*    PaPILO --- Parallel Presolve for Integer and Linear Optimization       */
+/*                                                                           */
+/* Copyright (C) 2020-2023 Konrad-Zuse-Zentrum                               */
+/*                     fuer Informationstechnik Berlin                       */
+/*                                                                           */
+/* This program is free software: you can redistribute it and/or modify      */
+/* it under the terms of the GNU Lesser General Public License as published  */
+/* by the Free Software Foundation, either version 3 of the License, or      */
+/* (at your option) any later version.                                       */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU Lesser General Public License for more details.                       */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this program.  If not, see <https://www.gnu.org/licenses/>.    */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-Parts of the code were copied or adapted from MiniSat.
+#ifndef _PAPILO_INTERFACES_ROUNDINGSAT_INTERFACE_HPP_
+#define _PAPILO_INTERFACES_ROUNDINGSAT_INTERFACE_HPP_
 
-MiniSat -- Copyright (c) 2003-2006, Niklas Een, Niklas Sorensson
-           Copyright (c) 2007-2010  Niklas Sorensson
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-***********************************************************************/
-
+#include "Solver.hpp"
+#include "typedefs.hpp"
+#include "run.hpp"
 #include <csignal>
 #include <fstream>
 #include <memory>
-#include "auxiliary.hpp"
-#include "globals.hpp"
-#include "parsing.hpp"
-#include "run.hpp"
-
-
-namespace rs {
-
-bool asynch_interrupt;
-Options options;
-Stats stats;
-
-}  // namespace rs
-
-static void SIGINT_interrupt([[maybe_unused]] int signum) { rs::asynch_interrupt = true; }
-
-static void SIGINT_exit([[maybe_unused]] int signum) {
-  printf("\n*** INTERRUPTED ***\n");
-  exit(1);
-}
-
-int foo(int argc, char** argv) {
-  rs::stats.STARTTIME = rs::aux::cpuTime();
-  rs::asynch_interrupt = false;
-
-  signal(SIGINT, SIGINT_exit);
-  signal(SIGTERM, SIGINT_exit);
-  signal(SIGXCPU, SIGINT_exit);
-
-  rs::options.parseCommandLine(argc, argv);
-
-  if (rs::options.verbosity.get() > 0) {
-    std::cout << "c RoundingSat 2\n";
-    std::cout << "c branch " << EXPANDED(GIT_BRANCH) << "\n";
-    std::cout << "c commit " << EXPANDED(GIT_COMMIT_HASH) << std::endl;
-  }
-
-  rs::run::solver.init();
-  rs::CeArb objective = rs::run::solver.cePools.takeArb();
-
-  if (!rs::options.formulaName.empty()) {
-    std::ifstream fin(rs::options.formulaName);
-    if (!fin) rs::quit::exit_ERROR({"Could not open ", rs::options.formulaName});
-    rs::parsing::file_read(fin, rs::run::solver, objective);
-  } else {
-    if (rs::options.verbosity.get() > 0) std::cout << "c No filename given, reading from standard input" << std::endl;
-    rs::parsing::file_read(std::cin, rs::run::solver, objective);
-  }
-
-  signal(SIGINT, SIGINT_interrupt);
-  signal(SIGTERM, SIGINT_interrupt);
-  signal(SIGXCPU, SIGINT_interrupt);
-
-  rs::run::solver.initLP(objective);
-
-  rs::run::run(objective);
-}
-
 
 namespace papilo {
-template <typename REAL>
-class RoundingsatFactory : public SolverFactory<REAL>
-{
-   RoundingsatFactory() = default;
 
- public:
-   std::unique_ptr<SolverInterface<REAL>>
-   newSolver( VerbosityLevel verbosity ) const override
-   {
+   template<typename REAL>
+   class RoundingsatInterface : public SolverInterface<REAL> {
 
-      return nullptr;
-   }
+      int
+      doSetUp(const Problem<REAL> &problem, const Vec<int> &origRowMap,
+              const Vec<int> &origColMap) {
 
-   virtual void
-   add_parameters( ParameterSet& parameter ) const
-   {
-   }
-   static std::unique_ptr<SolverFactory<REAL>>
-   create()
-   {
-      return std::unique_ptr<SolverFactory<REAL>>( new RoundingsatFactory<REAL>() );
-   }
-};
+         rs::run::solver.init( );
+         rs::CeArb objective = rs::run::solver.cePools.takeArb( );
+         assert(objective->isReset( ));
+         rs::CeArb input = rs::run::solver.cePools.takeArb( );
+
+         const Vec<String> &varNames = problem.getVariableNames( );
+         const VariableDomains<REAL> &domains = problem.getVariableDomains( );
+         const Vec<REAL> &obj = problem.getObjective( ).coefficients;
+         const Vec<REAL> &rhs = problem.getConstraintMatrix( ).getRightHandSides( );
+         const Vec<REAL> &lhs = problem.getConstraintMatrix( ).getLeftHandSides( );
+         const auto consMatrix = problem.getConstraintMatrix( );
+
+         for( int row = 0; row < problem.getNRows( ); ++row )
+         {
+            auto row_coeff = problem.getConstraintMatrix( ).getRowCoefficients(row);
+            if( !consMatrix.getRowFlags( )[ row ].test(RowFlag::kLhsInf))
+            {
+               for( int j = 0; j < row_coeff.getLength( ); ++j )
+               {
+                  String var_name = varNames[ origColMap[ row_coeff.getIndices( )[ j ]]];
+                  assert(row_coeff.getIndices( )[ j ] != 0);
+                  assert(domains.isBinary(row_coeff.getIndices( )[ j ]));
+                  if( var_name.empty( ) || var_name[ 0 ] != 'x' )
+                  {
+                     fmt::print("Invalid literal token: {}", var_name);
+                     return -1;
+                  }
+
+                  rs::Lit var_index = atoi(var_name.substr(1).c_str( ));
+                  if( var_index < 1 )
+                  {
+                     fmt::print("Variable {} less than 1\n ", var_name);
+                     return -1;
+                  }
+                  //TODO:
+                  // assert(num.isInteger(row_coeff.getIndices()[j]));
+                  int coeff = ( int ) row_coeff.getValues( )[ j ];
+                  if( coeff < 0 )
+                     var_index = -var_index;
+                  rs::run::solver.setNbVars(std::abs(var_index), true);
+                  input->addLhs(coeff, var_index);
+               }
+               input->addRhs(lhs[ row ]);
+               if( rs::run::solver.addConstraint(input, rs::Origin::FORMULA).second == rs::ID_Unsat )
+               {
+                  fmt::print("An error occurred\n");
+                  return -1;
+               }
+            }
+            if( !consMatrix.getRowFlags( )[ row ].test(RowFlag::kRhsInf))
+            {
+               for( int j = 0; j < row_coeff.getLength( ); ++j )
+               {
+                  String var_name = varNames[ origColMap[ row_coeff.getIndices( )[ j ]]];
+                  assert(row_coeff.getIndices( )[ j ] != 0);
+                  assert(domains.isBinary(row_coeff.getIndices( )[ j ]));
+                  if( var_name.empty( ) || var_name[ 0 ] != 'x' )
+                  {
+                     fmt::print("Invalid literal token: {}", var_name);
+                     return -1;
+                  }
+                  rs::Lit var_index = atoi(var_name.substr(1).c_str( ));
+                  if( var_index < 1 )
+                  {
+                     fmt::print("Variable {} less than 1\n ", var_name);
+                     return -1;
+                  }
+                  //TODO:
+                  // assert(num.isInteger(row_coeff.getIndices()[j]));
+                  int coeff = ( int ) row_coeff.getValues( )[ j ];
+                  if( coeff < 0 )
+                     var_index = -var_index;
+                  rs::run::solver.setNbVars(std::abs(var_index), true);
+                  input->addLhs(coeff, var_index);
+               }
+               input->addRhs(rhs[ row ]);
+               input->invert( );
+               if( rs::run::solver.addConstraint(input, rs::Origin::FORMULA).second == rs::ID_Unsat )
+               {
+                  fmt::print("An error occurred\n");
+                  return -1;
+               }
+            }
+
+         }
+      }
+
+      int
+      doSetUp(const Problem<REAL> &problem, const Vec<int> &origRowMap,
+              const Vec<int> &origColMap, const Components &components,
+              const ComponentInfo &component) {
+      }
+
+   public:
+      RoundingsatInterface( ) { }
+
+      void
+      setUp(const Problem<REAL> &prob, const Vec<int> &row_maps,
+            const Vec<int> &col_maps, const Components &components,
+            const ComponentInfo &component) override {
+         if( doSetUp(prob, row_maps, col_maps, components, component) != 0 )
+            this->status = SolverStatus::kError;
+      }
+
+      void
+      setNodeLimit(int
+                   num) override {
+      }
+
+      void
+      setGapLimit(
+            const REAL &gaplim) override {
+      }
+
+      void
+      setSoftTimeLimit(double
+                       tlim) override {
+      }
+
+      void
+      setTimeLimit(double
+                   tlim) override {
+      }
+
+      void
+      setVerbosity(VerbosityLevel
+                   verbosity) override {
+      }
+
+      void
+      setUp(const Problem<REAL> &prob, const Vec<int> &row_maps,
+            const Vec<int> &col_maps) override {
+         if( doSetUp(prob, row_maps, col_maps) != 0 )
+            this->status = SolverStatus::kError;
+      }
+
+      void
+      solve( )
+      override {
+      }
+
+      REAL
+      getDualBound( )
+      override {
+         return 0;
+      }
+
+      bool
+      getSolution(Solution<REAL> &solbuffer)
+      override {
+         return false;
+      }
+
+      bool
+      getSolution(const Components &components, int component,
+                  Solution<REAL> &solbuffer) override {
+
+         return false;
+      }
+
+      SolverType
+      getType( )
+      override {
+         return SolverType::PseudoBoolean;
+      }
+
+      String
+      getName( )
+      override {
+         return "RoundingSat";
+      }
+
+      void
+      printDetails( )
+      override {
+      }
+
+      bool
+      is_dual_solution_available( )
+      override {
+         return false;
+      }
+
+      void
+      addParameters(ParameterSet &paramSet)
+      override {
+      }
+
+      ~RoundingsatInterface( ) =
+      default;
+
+   };
+
+   template<typename REAL>
+   class RoundingsatFactory : public SolverFactory<REAL> {
+      RoundingsatFactory( ) = default;
+
+   public:
+      std::unique_ptr<SolverInterface<REAL>>
+      newSolver(VerbosityLevel verbosity) const override {
+         auto satsolver = std::unique_ptr<SolverInterface<REAL>>(
+               new RoundingsatInterface<REAL>( ));
+
+         return std::move(satsolver);
+      }
+
+      virtual void
+      add_parameters(ParameterSet &parameter) const {
+      }
+
+      static std::unique_ptr<SolverFactory<REAL>>
+      create( ) {
+         return std::unique_ptr<SolverFactory<REAL>>(
+               new RoundingsatFactory<REAL>( ));
+      }
+   };
 
 } // namespace papilo
+
+#endif
