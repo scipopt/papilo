b559d6739531bab79cd8c0d96659a696c38e8c3b
Alexander Hoen <hoen@zib.de>
2023-02-16 12:21:26 +0100

    VeriPB: slight change in run file

--- check/VeriPB/run_veripb.sh
+++ check/VeriPB/run_veripb.sh
@@ -3,5 +3,5 @@ echo "${FILENAME}"
 instance=${FILENAME%.pbp}.opb
 #  sbatch -c "veripb --trace --useColor ${instance} ${f} > ${out}"
 echo ">>> Executing: veripb --trace --useColor ${instance} ${FILENAME}"
-source "/scratch/opt/ahoen/VeriPB-master/z1venv/bin/activate"
+source "/scratch/opt/ahoen/VeriPB/z1venv/bin/activate"
 eval "veripb --trace --useColor ${instance} ${FILENAME}"
