1d54446c7dd7ea9827bd70f1678915566de70f67
Alexander Hoen <hoen@zib.de>
2023-12-14 16:59:31 +0100

    Change name in header to Zuse Institute Berlin and year to 2023

--- src/papilo/interfaces/SolverInterface.hpp
+++ src/papilo/interfaces/SolverInterface.hpp
@@ -3,8 +3,7 @@
 /*               This file is part of the program and library                */
 /*    PaPILO --- Parallel Presolve for Integer and Linear Optimization       */
 /*                                                                           */
-/* Copyright (C) 2020-2023 Konrad-Zuse-Zentrum                               */
-/*                     fuer Informationstechnik Berlin                       */
+/* Copyright (C) 2020-2023 Zuse Institute Berlin (ZIB)                       */
 /*                                                                           */
 /* This program is free software: you can redistribute it and/or modify      */
 /* it under the terms of the GNU Lesser General Public License as published  */
