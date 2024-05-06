6c547ed2fb4e00c02bfa0fa48d9da6f326eb564e
Julian Manns <manns@zib.de>
2024-05-06 13:38:37 +0200

    Update Config.hpp in change_version.sh

--- scripts/change_version.sh
+++ scripts/change_version.sh
@@ -7,9 +7,10 @@ do
    version[$i]=0
 done
 
-versionstring="${version[0]}.${version[1]}.${version[2]}.${version[3]}"
+versionstring="${version[0]}.${version[1]}.${version[2]}"
 cmakefile=`dirname $0`/../CMakeLists.txt
 dummyconfig=`dirname $0`/../src/papilo/Config.hpp
+makeproject=`dirname $0`/../make/make.project
 
 sed -i "s/^set(PAPILO_VERSION_MAJOR .*/set(PAPILO_VERSION_MAJOR ${version[0]})/g" $cmakefile
 sed -i "s/^set(PAPILO_VERSION_MINOR .*/set(PAPILO_VERSION_MINOR ${version[1]})/g" $cmakefile
@@ -19,3 +20,5 @@ sed -i "s/^#define PAPILO_VERSION_MAJOR .*/#define PAPILO_VERSION_MAJOR ${versio
 sed -i "s/^#define PAPILO_VERSION_MINOR .*/#define PAPILO_VERSION_MINOR ${version[1]}/g" $dummyconfig
 sed -i "s/^#define PAPILO_VERSION_PATCH .*/#define PAPILO_VERSION_PATCH ${version[2]}/g" $dummyconfig
 sed -i "s/^#define PAPILO_VERSION_TWEAK .*/#define PAPILO_VERSION_TWEAK ${version[3]}/g" $dummyconfig
+
+sed -i "s/VERSION	=	.*/VERSION	=	${versionstring}/g" $makeproject
