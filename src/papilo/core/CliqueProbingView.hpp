4656b80f33c2e13a74d3c3f08b03969d1ad16351
Jacob von Holly <von-holly-ponientzietz@campus.tu-berlin.de>
2025-08-01 13:19:48 +0200

    Fixing assertion fail

--- src/papilo/core/CliqueProbingView.hpp
+++ src/papilo/core/CliqueProbingView.hpp
@@ -97,6 +97,7 @@ class CliqueProbingView
       Vec<std::pair<int,int>>& ub_implications_thread_local, Vec<int>& fix_to_zero_thread_local, bool& cliqueEquation, Vec<int>& indices,
       const int& clique, const Vec<int>& binary_indices, const int& len )
    {
+      assert( indices.size() > 0 );
       if( &probingClique != &clique )
          probingClique = clique;
       if( &cliqueind != &indices)
