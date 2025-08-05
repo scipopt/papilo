206f6b8ef4c76c8c0dc0ea68610f9ad8b62d47d1
Jacob von Holly <von-holly-ponientzietz@campus.tu-berlin.de>
2025-08-05 15:42:34 +0200

    Debugging Nondeterminism through tests

--- test/papilo/presolve/ProbingTest.cpp
+++ test/papilo/presolve/ProbingTest.cpp
@@ -151,7 +151,7 @@ TEST_CASE( "clique-probing-1", "[presolve]" )
    REQUIRE( reductions.getReduction( 3 ).row == -1 );
    REQUIRE( reductions.getReduction( 3 ).newval == 0 );
 
-   REQUIRE( false )
+   REQUIRE( false );
 
 #endif
 
@@ -236,7 +236,7 @@ TEST_CASE( "clique-probing-2", "[presolve]" )
    REQUIRE( reductions.getReduction( 9 ).row == -1 );
    REQUIRE( reductions.getReduction( 9 ).newval == 0 );
 
-   REQUIRE( false )
+   REQUIRE( false );
 
 #endif
 
